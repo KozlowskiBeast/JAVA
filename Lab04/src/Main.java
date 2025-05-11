f76720384de83e1285338802fc54d164423c6d9e
Kacper <kacper3008kox124@interia.pl>
2025-05-11 19:42:32 +0200

    Add files via upload

---
+++ Lab04/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Osoba janKowalski = new Osoba("Jan", "Kowalski", 34);
+        Osoba cezaryPuzura = new Osoba("Cezary", "Pazura", 51);
+        Osoba donaldTrump = new Osoba("Donald", "Trump", 80);
+        janKowalski.pokazDane();
+        cezaryPuzura.pokazDane();
+        donaldTrump.pokazDane();
+
+        Student student1 = new Student("Kamil", "Nowak", "Informatyka", 123456, 2);
+        Student student2 = new Student("Artur", "Kamiński", "Matematyka", 112379, 3);
+        Student student3 = new Student("Ala", "Kot", "Ekonomia", 127590, 2);
+        Student student4 = new Student("Jon", "Arbuckle", "Informatyka i Ekonometria", 112042, 4);
+        student1.pokazDane();
+        student2.pokazDane();
+        student3.pokazDane();
+        student4.pokazDane();
+    }
+}
---
+++ Lab04/src/Student.java
@@ -0,0 +1,75 @@
+public class Student {
+    private String imie, nazwisko, nazwaSpecjalnosci;
+    private int nr_indeksu, rokStudiow;
+
+    public Student(String imie, String nazwisko, String nazwaSpecjalnosci, int nr_indeksu, int rokStudiow) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nazwaSpecjalnosci = nazwaSpecjalnosci;
+        this.nr_indeksu = nr_indeksu;
+        this.rokStudiow = rokStudiow;
+    }
+
+
+    public Student(String imie, String nazwisko, String nazwaSpecjalnosci, int nr_indeksu) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nazwaSpecjalnosci = nazwaSpecjalnosci;
+        this.nr_indeksu = nr_indeksu;
+    }
+
+    public Student(String nazwaSpecjalnosci, String nazwisko, String imie) {
+        this.nazwaSpecjalnosci = nazwaSpecjalnosci;
+        this.nazwisko = nazwisko;
+        this.imie = imie;
+    }
+
+    public Student(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    public void pokazDane(){
+        System.out.println(this.imie + " " + this.nazwisko + ", " + this.nr_indeksu + ", " + this.nazwaSpecjalnosci +  ", rok: " +  this.rokStudiow);
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getNazwaSpecjalnosci() {
+        return nazwaSpecjalnosci;
+    }
+
+    public void setNazwaSpecjalnosci(String nazwaSpecjalnosci) {
+        this.nazwaSpecjalnosci = nazwaSpecjalnosci;
+    }
+
+    public int getNr_indeksu() {
+        return nr_indeksu;
+    }
+
+    public void setNr_indeksu(int nr_indeksu) {
+        this.nr_indeksu = nr_indeksu;
+    }
+
+    public int getRokStudiow() {
+        return rokStudiow;
+    }
+
+    public void setRokStudiow(int rokStudiow) {
+        this.rokStudiow = rokStudiow;
+    }
+}
